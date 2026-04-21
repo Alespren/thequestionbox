c97005d1e14dbca5eee2fee6547f3ff411e16a60
Ashley <corbinped@gmail.com>
2026-04-20 18:40:14 -0600

    Refactor

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import express from "express";
-import fs from "node:fs/promises";
-import path from "node:path";
-import { parse } from "node-html-parser";
-import wkhtmltoimage from "wkhtmltoimage";
+import { manualConfig } from "./manual-config.js";
+import { getRuleImage } from "./rule-image.js";
 
 const app = express();
 const port = 3000;
 
-const manualPath = "./DECODE_Competition_Manual_TU32.htm";
-const manualAssetsDir = path.resolve("./DECODE_Competition_Manual_TU32_files");
 const manualAssetsBaseUrl = `http://localhost:${port}/`;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.use(
-  "/DECODE_Competition_Manual_TU32_files",
-  express.static(manualAssetsDir),
-);
+app.use(manualConfig.assetsRoute, express.static(manualConfig.assetsDir));
 
 app.get("/rule/:ruleNum", async (req, res, next) => {
   if (!isValidRule(req.params.ruleNum)) {
@@ -27,7 +20,12 @@ app.get("/rule/:ruleNum", async (req, res, next) => {
   }
 
   res.type("png");
-  res.send(await getRuleImage(req.params.ruleNum));
+  res.send(
+    await getRuleImage(req.params.ruleNum, {
+      manualPath: manualConfig.path,
+      baseHref: manualAssetsBaseUrl,
+    }),
+  );
 });
 
 app.use((req, res, next) => {
@@ -40,43 +38,4 @@ app.listen(port, () => {
 
 function isValidRule(ruleNum) {
   return true;
-}
-
-async function getRuleImage(ruleNum) {
-  // Parse HTML
-  const file = await fs.readFile(manualPath);
-  const root = parse(file);
-
-  const rule = root.querySelector(`a[name=${ruleNum}]`);
-
-  let next = rule.parentNode;
-  const nodes = [next];
-  next = next.nextElementSibling;
-
-  while (
-    next != null &&
-    !(
-      next.classNames.includes("RuleNumber") ||
-      next.classNames.includes("TRules")
-    )
-  ) {
-    nodes.push(next);
-    next = next.nextElementSibling;
-  }
-
-  const baseHref = manualAssetsBaseUrl;
-
-  const styleTags = root.querySelectorAll("style");
-  const styleHtml = styleTags.map((tag) => tag.toString()).join("");
-  const bodyHtml = nodes.map((node) => node.toString()).join("");
-  const stringRepresentation = `<!doctype html><html><head><base href="${baseHref}">${styleHtml}</head><body>${bodyHtml}</body></html>`;
-
-  const stream = wkhtmltoimage.generate(stringRepresentation);
-
-  return await new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-    stream.on("error", reject);
-  });
 }
---
+++ manual-config.js
@@ -0,0 +1,7 @@
+import path from "node:path";
+
+export const manualConfig = {
+  path: "./DECODE_Competition_Manual_TU32.htm",
+  assetsDir: path.resolve("./DECODE_Competition_Manual_TU32_files"),
+  assetsRoute: "/DECODE_Competition_Manual_TU32_files",
+};
